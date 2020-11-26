f17ee59592c9e1e554e2c55a68dcd2ae1b534c0e
rizki87 <rizkipratama87@gmail.com>
2020-11-26 21:05:25 +0700

    Exercise, 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,14 @@ import Person from './components/person'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Helas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filteredPerson, setFilteredPerson ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -40,9 +44,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    // console.log(event.target.value)
+    setFilteredPerson(event.target.value)    
+  }
+
+  const filteredList = !filteredPerson ? persons : persons.filter(person => 
+    person.name.includes(filteredPerson) 
+  );
+  // console.log("filteredList ", filteredList)
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+      filter shown with <input onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -56,7 +74,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, i) => 
+        {filteredList.map((person, i) => 
           <Person key={i} person={person} />
         )}
       </ul>
